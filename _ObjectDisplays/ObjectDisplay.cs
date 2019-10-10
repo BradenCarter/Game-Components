4f69c75fdc016edeb091ccb87c6cd57059ab6888
BradenCarter <bradencarter@outlook.com>
2019-10-10 12:31:34 -0400

    Object Display Setup

---
+++ _ObjectDisplays/ObjectDisplay.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ObjectDisplay<T, H> : MonoBehaviour where H : IObjectDisplay<T>
+{
+    private H[] objDisplays;
+
+    public T item { get; protected set; }
+
+    public event Action<T> OnObjectSet;
+
+    private List<H> updatedDisplays = new List<H>();
+
+    private void Awake()
+    {
+        GetDisplays();
+    }
+
+    public void UpdateInfoDisplays()
+    {
+        updatedDisplays.Clear();
+        GetDisplays();
+        for (int i = 0; i < objDisplays.Length; i++)
+        {
+            if (updatedDisplays.Contains(objDisplays[i]) == false)
+            {
+                objDisplays[i].Set(item);
+                updatedDisplays.Add(objDisplays[i]);
+            }
+        }
+    }
+
+    private void GetDisplays()
+    {
+        if (objDisplays == null)
+            objDisplays = GetComponentsInChildren<H>(true);
+    }
+
+    public virtual void Set(T item)
+    {
+        this.item = item;
+        OnObjectSet?.Invoke(item);
+        UpdateInfoDisplays();
+    }
+}
